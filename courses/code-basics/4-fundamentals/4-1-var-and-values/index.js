38803a6b17b0b2509119c56ff7299b06fa47da6b
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-16 17:23:36 +0300

    [LES-4.1/st-compl] var-and-values
    Intro to variables declaration, only "let" or "const" (no "var").
    
    purple-school

---
+++ courses/code-basics/4-fundamentals/4-1-var-and-values/index.js
@@ -0,0 +1,11 @@
+let a = 1;
+console.log(a); // 1
+
+a = 'test';
+console.log(a); // test
+
+const b = 2;
+console.log(b); // 2
+
+b = 'test2';
+console.log(b); // TypeError: Assignment to constant variable.
