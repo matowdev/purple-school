0574b974ae0421a68cb9faf0b3e85ff92da2620e
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-17 16:53:04 +0300

    [LES-4.2/st-compl] arithmetic-operators
    Working out the simplest arithmetic operations with num's/str's.
    
    Worth noting:
      - str, num addition/concatenation.
    
    purple-school

---
+++ courses/code-basics/4-fundamentals/4-2-arithmetic-operators/index.js
@@ -0,0 +1,18 @@
+// операции с числами
+let width = 5;
+let height = 10;
+let area = width * height;
+console.log(area); // 50 (т.е. 5 * 10)
+
+let division = height / width;
+console.log(division); // 2
+
+let power = 3 * 3 * 3;
+let power2 = 3 ** 3;
+let power3 = Math.pow(3, 3);
+console.log(power3); // 27
+
+// операции со строками
+let city = 'Vitebsk';
+let street = 'Mira';
+console.log(city + ', ' + street + ' 21'); // Vitebsk, Mira 21
