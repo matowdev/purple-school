11b3929b0895e2400b4d491b0d42b93daf7ec063
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-22 15:15:17 +0300

    [LES-4.3/st-compl] assign-comparison-operators
    Working with short form of assign.. increment/decrement. Boolean logic.
    
    Worth noting:
      - prefix/postfix form of the entry, for increment/decrement.
    
    purple-school

---
+++ courses/code-basics/4-fundamentals/4-3-assign-comparison-operators/index.js
@@ -0,0 +1,19 @@
+// операции с присвоением (краткая форма)
+let age = 18 + 3; // 21
+age += 2; // age = age + 2 = 23
+age -= 3; // age = age - 3 = 20
+age *= 2; // age = age * 2 = 40
+age /= 2; // age = age / 2 = 20
+
+// инкремент/декремент
+age++; // age = age + 1 = 21
+age--; // age = age - 1 = 20
+console.log(age);
+
+// операции сравнения (булева логика)
+const personAge = 33;
+console.log(personAge > age); // 33 > 20, true
+console.log(personAge >= age); // true
+console.log(personAge < age); // false
+console.log(personAge <= age); // false
+console.log(personAge == age); // false
