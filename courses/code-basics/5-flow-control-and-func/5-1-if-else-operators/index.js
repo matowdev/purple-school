475087e8c9a49c4537fb0997caa19e7f908e0e76
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-02-12 13:56:18 +0300

    [LES-5.1/st-compl] if-else-operators
    Interaction with if..else operator(s), code execution conditions.
    
    Worth noting:
      - in general, conditional statements.
    
    purple-school

---
+++ courses/code-basics/5-flow-control-and-func/5-1-if-else-operators/index.js
@@ -0,0 +1,10 @@
+// оператор(ы) if..else, условия для выполнения кода (conditional statements)
+let userMoney = 75;
+
+if (userMoney > 100) {
+  console.log('Пользователь может купить товар из "High price" сегмента!');
+} else if (userMoney > 50) {
+  console.log('Пользователь может купить товар из "Medium price" сегмента..'); // Пользователь может купить товар из "Medium price" сегмента..
+} else {
+  console.log('Пользователь может купить товар из "Low price" сегмента((');
+}
