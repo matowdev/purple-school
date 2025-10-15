aadcc0082fb546a520bd66825b70dfa09f31d254
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2025-10-15 14:56:25 +0300

    [PROJ-18.10/st] adding-days-comments
    Adding/copying files from the previous realization.
    
    FS-dev: B-3 / JS basic

---
+++ full-stack-dev/3-js-basic/18-proj-habit-tracker/18-10-adding-days-comments/js/index.js
@@ -0,0 +1,162 @@
+'use strict';
+
+let habbits = [];
+const HABBITS_KEY = 'HABBITS';
+
+// app
+const app = {
+  'menu-list': document.getElementById('sidebar-list'),
+  header: {
+    title: document.getElementById('header-title'),
+    progressPercent: document.getElementById('progress-percent'),
+    progressValue: document.getElementById('progress-value'),
+  },
+  'habbit-days': {
+    list: document.getElementById('habbit-days-list'),
+  },
+};
+
+// ** utility **
+function loadData() {
+  const habbitsStr = localStorage.getItem(HABBITS_KEY);
+  const habbitsArr = JSON.parse(habbitsStr);
+
+  if (Array.isArray(habbitsArr)) {
+    habbits = habbitsArr;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABBITS_KEY, JSON.stringify(habbits));
+}
+
+// ** render **
+function rerenderMenu(activeHabbit) {
+  for (const habbit of habbits) {
+    const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`); // определение/флаг.. есть уже элемент (создавался) или нет
+
+    if (!existed) {
+      // если НЕТ.. создание li/элемента sidebar/меню.. привычки (согласно data)
+      const habbitItem = document.createElement('li');
+      habbitItem.classList.add('sidebar__nav-item');
+
+      const habbitBtn = document.createElement('button');
+      habbitBtn.classList.add('sidebar__nav-btn');
+      habbitBtn.setAttribute('type', 'button');
+      habbitBtn.setAttribute('aria-label', 'Выбрать привычку');
+      habbitBtn.setAttribute('title', habbit.title);
+      habbitBtn.setAttribute('menu-habbit-id', habbit.id);
+      habbitBtn.innerHTML = `<img class="sidebar__nav-icon" src="./images/${habbit.icon}.svg" width="${habbit.width}" height="${habbit.height}" alt="Иконка: ${habbit.name}">`;
+
+      habbitBtn.addEventListener('click', () => rerender(habbit.id)); // тонкий момент.. по сути "замыкание" для каждой кнопки/её ID.. потом передача именно его/себя в rerender()
+
+      // проверка на активность (при создании)
+      if (activeHabbit.id === habbit.id) {
+        habbitBtn.classList.add('sidebar__nav-btn_active');
+      }
+
+      habbitItem.append(habbitBtn);
+      app['menu-list'].append(habbitItem);
+    } else {
+      // если ЕСТЬ.. только обновление "активного" класса
+      if (activeHabbit.id === habbit.id) {
+        existed.classList.add('sidebar__nav-btn_active');
+      } else {
+        existed.classList.remove('sidebar__nav-btn_active');
+      }
+    }
+  }
+}
+
+function rerenderHeaderContentEl(activeHabbit) {
+  app.header.title.textContent = activeHabbit.title; // добавление/обновление заголовка
+
+  // подсчёт прогресса/выполнения (согласно "уже" отмеченных дней и "ранее" поставленной цели/дней)
+  const progress =
+    activeHabbit.days.length / activeHabbit.target > 1
+      ? 100
+      : (activeHabbit.days.length / activeHabbit.target) * 100;
+
+  app.header.progressPercent.textContent = `${progress.toFixed(0)}%`;
+  app.header.progressValue.setAttribute(
+    'style',
+    `width: ${progress.toFixed(0)}%`
+  );
+}
+
+function rerenderHabbitDaysContentEl(activeHabbit) {
+  app['habbit-days'].list.innerHTML = ''; // предварительная очистка списка (всё будет создаваться заново)
+
+  const docFragment = document.createDocumentFragment(); // создание промежуточного/специального контейнера (сразу в него.. потом уже в document)
+
+  // создание/отрисовка уже существующих дней (согласно массива days)
+  for (let i = 0; i < activeHabbit.days.length; i++) {
+    const habbitDaysItem = document.createElement('li');
+    habbitDaysItem.classList.add('habbit-days__item');
+
+    habbitDaysItem.innerHTML = `<div class="habbit-days__label">День ${
+      i + 1
+    }</div>
+      <div class="habbit-days__comment-wrap">
+        <div class="habbit-days__comment">${activeHabbit.days[i].comment}</div>
+        <button class="habbit-days__delete-btn" type="button" aria-label="Удалить день" title="Удалить день">
+          <img class="habbit-days__delete-icon" src="./images/trash-icon.svg" width="24" height="24"
+            alt="Иконка: Мусорное ведро">
+        </button>
+      </div>`;
+
+    docFragment.append(habbitDaysItem); // сразу добавление в "docFragment"
+  }
+
+  // "отдельное" создание/отрисовка крайнего дня с комментарием (пустым)
+  const habbitDaysCommentItem = document.createElement('li');
+  habbitDaysCommentItem.classList.add(
+    'habbit-days__item',
+    'habbit-days__item-comment'
+  );
+
+  habbitDaysCommentItem.innerHTML = `<div class="habbit-days__label">День ${
+    activeHabbit.days.length + 1
+  }</div>
+  <form class="habbit-days__form" id="comment-form" onsubmit="addCommentDay(event)">
+    <input class="habbit-days__form-input" id="comment-input" type="text" name="comment-day" placeholder="Комментарий">
+    <button class="habbit-days__form-btn" type="submit" title="Добавить комментарий">Готово</button>
+  </form>`;
+
+  docFragment.append(habbitDaysCommentItem);
+  app['habbit-days'].list.append(docFragment); // и всё за раз на страницу/в DOM-дерево
+}
+
+// поиск/определение "активной" привычки.. запуск отрисовок элементов/переключение активностей
+function rerender(activeHabbitId) {
+  const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
+
+  if (!activeHabbit) {
+    return;
+  }
+
+  rerenderMenu(activeHabbit);
+  rerenderHeaderContentEl(activeHabbit);
+  rerenderHabbitDaysContentEl(activeHabbit);
+}
+
+// ** business **
+function addCommentDay(event) {
+  event.preventDefault(); // отмена default отправки/перезагрузки страницы
+
+  const formData = new FormData(event.target); // создание объекта данных/возможностей согласно искомой формы (полученной через onsubmit/event.target)
+  const comment = formData.get('comment-day'); // получение значения/комментария из инпута согласно его имени/name
+
+  console.log(comment); // Уже лучше!
+}
+
+// init
+(() => {
+  // загрузка данных.. по сути автоматическая/сразу (соответственно через IIFE)
+  loadData();
+
+  // запуск работы с sidebar/меню..
+  if (habbits.length > 0) {
+    rerender(habbits[0].id); // пока.. принудительно передаёт
+  }
+})();
