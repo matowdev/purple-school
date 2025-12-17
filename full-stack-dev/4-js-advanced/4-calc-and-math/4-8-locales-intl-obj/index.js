53734efa52ee2ceece70ebd857fd736cecfef04a
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2025-12-17 15:39:03 +0300

    [LES-4.8/st-compl] locales-intl-obj
    Working with "Intl obj, NumberFormat() constructor".
    
    Worth noting:
      - this "Internationalization API".
    
    FS-dev: B-4 / JS advanced

---
+++ full-stack-dev/4-js-advanced/4-calc-and-math/4-8-locales-intl-obj/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const options1 = {
+  style: 'currency',
+  currency: 'RUB',
+  // useGrouping: false,
+};
+
+const options2 = {
+  style: 'currency',
+  currency: 'USD',
+};
+
+const options3 = {
+  style: 'decimal',
+};
+
+const options4 = {
+  style: 'percent',
+};
+
+const options5 = {
+  style: 'unit',
+  unit: 'celsius',
+};
+
+console.log(new Intl.NumberFormat('ru-RU', options1).format(23000)); // "23 000,00 ₽"
+console.log(new Intl.NumberFormat('en-US', options2).format(23000)); // "$23,000.00"
+console.log(new Intl.NumberFormat('ru-RU', options3).format(10000)); // "10 000"
+console.log(new Intl.NumberFormat('ru-RU', options4).format(0.1)); // "10 %"
+console.log(new Intl.NumberFormat('ru-RU', options5).format(25)); // "25 °C"
